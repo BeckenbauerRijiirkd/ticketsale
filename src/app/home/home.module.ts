4acc780f717c4b741f2b82900b6e8117c9f60c66
Beck Raika <beckraika@gmail.com>
2022-01-18 08:23:40 -0300

    tela login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
