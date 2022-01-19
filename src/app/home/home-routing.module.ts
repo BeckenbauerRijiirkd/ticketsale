5b0212f7772e1021a348f7b384f89b8a452f5a32
Beck Raika <beckraika@gmail.com>
2022-01-19 11:27:44 -0300

    Fomulario

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,15 +8,21 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children:[{
-      path:'',
-      component: LoginComponent
-    }]
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path:"novousuario",
+        component: NovoUsuarioComponent
+      }
+    ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
