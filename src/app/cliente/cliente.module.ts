460779c3b185e776603dbdf549cd644455d32327
beckraika <beckraika@gmail.com>
2022-01-24 21:34:30 -0300

    client page

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ClienteRoutingModule } from './cliente-routing.module';
 import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
+import { ClienteComponent } from './cliente.component';
 
 
 @NgModule({
-  declarations: [ListaClienteComponent],
+  declarations: [ListaClienteComponent, ClienteComponent],
   imports: [
     CommonModule,
     ClienteRoutingModule
