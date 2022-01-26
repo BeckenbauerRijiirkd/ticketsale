f1bc76b11b74de82d2dac38a0ebd232d4ca47a39
Beck Raika <beckraika@gmail.com>
2022-01-26 10:51:14 -0300

    service event

--- src/app/eventos/eventos.service.ts
+++ src/app/eventos/eventos.service.ts
@@ -1,9 +1,21 @@
+import { TokenService } from './../autenticacao/token.service';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Eventos } from './eventos';
 
+const API = `http://localhost:8080`;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventosService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  constructor() { }
+  listaDeEventos(): Observable<Eventos> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Eventos>(`${API}/eventos`, {
+      headers,
+    });
+  }
 }
