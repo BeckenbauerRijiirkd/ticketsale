b86e30320c1b94cda0d6e6dda8206651ce1f9a13
Bruno Batista <brunobatysta.fla@gmail.com>
2022-03-05 18:27:08 -0300

    Clear

--- src/app/componentes/rodape/rodape.module.ts
+++ src/app/componentes/rodape/rodape.module.ts
@@ -2,10 +2,11 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RodapeComponent } from './rodape.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [RodapeComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule],
   exports: [RodapeComponent],
 })
 export class RodapeModule {}
