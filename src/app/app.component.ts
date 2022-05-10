5f22890face9f71e5f362d637ce66d67593794d6
Beck Raika <beckraika@gmail.com>
2022-05-10 14:21:27 -0300

    sidebar teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ticketsale';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ONINIT');
+    var menu_btn = document.querySelector('#menu-btn');
+    var sidebar = document.querySelector('#sidebar');
+    var container = document.querySelector('.my-container');
+    sidebar?.classList.toggle('active-nav');
+    container?.classList.toggle('active-cont');
+    console.log(menu_btn);
+    console.log('menu_btn');
+    menu_btn?.addEventListener('click', () => {
+      sidebar?.classList.toggle('active-nav');
+      container?.classList.toggle('active-cont');
+    });
+  }
 }
