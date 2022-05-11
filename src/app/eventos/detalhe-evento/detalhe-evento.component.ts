607222c89a324d492848f6a62b8e88ac2c6a8a03
Beck Raika <beckraika@gmail.com>
2022-05-11 12:31:14 -0300

    component create

---
+++ src/app/eventos/detalhe-evento/detalhe-evento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-evento',
+  templateUrl: './detalhe-evento.component.html',
+  styleUrls: ['./detalhe-evento.component.css']
+})
+export class DetalheEventoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
