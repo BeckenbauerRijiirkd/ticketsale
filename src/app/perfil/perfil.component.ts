8a0584bef2a16d80b10cc0913e183517cf5a5959
Setyns <cleitonmpc91@gmail.com>
2022-05-18 21:57:04 -0300

    mudanças feitas no perfil.component.ts e no html

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,9 +1,10 @@
-import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
-import { Usuario } from './../autenticacao/usuario/usuario';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { PerfilService } from './perfil.service';
 import { Perfil } from './perfil';
 import { Observable } from 'rxjs';
+import { FormBuilder,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-perfil',
@@ -11,15 +12,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./perfil.component.css'],
 })
 export class PerfilComponent implements OnInit {
-  nome = '';
+  nome = [''];
   perfil$!: Observable<Perfil>;
-  cpf: '' = "";
-  cidade: '' = "";
-  uf: '' = "";
-  compras: '' = "";
+  cpf = ['']
+  cidade = ['']
+  uf = ['']
+  compras = ['']
 
 
-  constructor(private perfilService: PerfilService) {}
+  constructor(
+    private perfilService: PerfilService,
+    private formBuilder: FormBuilder,
+    private router: Router
+    )
+    {}
 
   ngOnInit(): void {
     this.perfil$ = this.perfilService.getPerfil(1);
