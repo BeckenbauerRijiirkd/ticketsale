e92845d6cc36bfce7888927a215224b47dd4e6a2
Beck Raika <beckraika@gmail.com>
2022-06-01 07:39:46 -0300

    edit perfil

--- src/app/perfil/perfil.service.ts
+++ src/app/perfil/perfil.service.ts
@@ -1,7 +1,7 @@
 import { Usuario } from './../autenticacao/usuario/usuario';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Perfil } from './perfil';
 import { environment } from 'src/environments/environment';
@@ -13,14 +13,19 @@ const API = environment.apiURL;
   providedIn: 'root',
 })
 export class PerfilService {
-  
   // user$ = this.usuarioService.retornaUsuario()
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   getPerfil(id: number): Observable<Perfil> {
     return this.http.get<Perfil>(`${API}/clientes/${id}`);
+  }
+
+  putPerfil(id: number, perfil: Perfil) {
+    console.log(perfil);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json; charset=utf-8',
+    });
+    return this.http.put(`${API}/clientes/${id}`, perfil, { headers: headers });
   }
 }
